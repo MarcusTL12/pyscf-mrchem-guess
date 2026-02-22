c61276edf43fee870ef2cda8fd386da2827ff82f
MarcusTL12 <marcuspaafjellet@gmail.com>
2026-02-22 22:28:25 +0100

    Printing bas file from orca

--- from_orca.py
+++ from_orca.py
@@ -66,15 +66,61 @@ def read_basis(orca_file):
     return basis
 
 
+def print_mrchem_bas_file(atoms, basis):
+    with open("mrchem.bas", "w") as f:
+        f.write("Gaussian basis from ORCA output file\n")
+        f.write(f"{len(basis):9}\n")
+
+        atom_coord_dict = {}
+
+        for symbol, q, coord in atoms:
+            if symbol in atom_coord_dict:
+                atom_coord_dict[symbol][1].append(coord)
+            else:
+                atom_coord_dict[symbol] = (q, [coord])
+
+        for symbol in atom_coord_dict:
+            q, coords = atom_coord_dict[symbol]
+            f.write(f"{q:9}. {len(coords):4}")
+            funcs_per_shell = {}
+            for angmom in basis[symbol]:
+                if angmom in funcs_per_shell:
+                    funcs_per_shell[angmom] += 1
+                else:
+                    funcs_per_shell[angmom] = 1
+            print(funcs_per_shell)
+            f.write(f" {len(basis[symbol]):4}")
+            for angmom in basis[symbol]:
+                f.write(f" {len(basis[symbol][angmom]):4}")
+            f.write("\n")
+
+            for coord in coords:
+                f.write(f"{symbol} ")
+                for x in coord:
+                    f.write(f" {x:18.10f}")
+                f.write("\n")
+
+            for angmom in basis[symbol]:
+                for exps, coeffs in basis[symbol][angmom]:
+                    n_prim = len(exps)
+                    n_cont = len(coeffs)
+
+                    f.write(f"{n_prim:9} {n_cont:4}\n")
+                    for i in range(n_prim):
+                        f.write(f"{exps[i]:15.7f}")
+                        for j in range(n_cont):
+                            f.write(f" {coeffs[j][i]:11.8f}")
+                        f.write("\n")
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as orca_file:
         atoms = read_geometry(orca_file)
-
-        pprint(atoms)
-
         basis = read_basis(orca_file)
 
         print(basis)
+        print()
+
+        print_mrchem_bas_file(atoms, basis)
 
         # for l in orca_file:
         #     if l.strip() == "BASIS SET IN INPUT FORMAT":
